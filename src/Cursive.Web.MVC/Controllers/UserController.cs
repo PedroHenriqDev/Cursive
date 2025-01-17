b42a53d07f4d7ac572ef24c88b0ac9b09575770a
PedroHenriqDev <devpedrohenri@gmail.com>
2025-01-16 21:09:13 -0300

    Created Cursive Web Site with MVC design.

---
+++ src/Cursive.Web.MVC/Controllers/UserController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cursive.Web.MVC.Controllers;
+
+public class UserController : Controller
+{
+    [HttpGet]
+    public IActionResult Login() => View();
+}
