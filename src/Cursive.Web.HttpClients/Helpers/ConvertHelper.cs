1e4ed3e93251d1d7652d5dfad32015bea4f72a25
PedroHenriqDev <devpedrohenri@gmail.com>
2025-01-18 20:46:33 -0300

    API request logic has been encapsulated in a dedicated project.

---
+++ src/Cursive.Web.HttpClients/Helpers/ConvertHelper.cs
@@ -0,0 +1,19 @@
+﻿using Cursive.Communication.Dtos;
+using Cursive.Communication.Dtos.Interfaces;
+using Newtonsoft.Json;
+
+namespace Cursive.Web.HttpClients.Helpers;
+
+public static class ConvertHelper
+{
+    public static StringContent ConvertToStringContent(object objToConvert)
+    {
+        return new StringContent(JsonConvert.SerializeObject(objToConvert), System.Text.Encoding.UTF8, "application/json");
+    }
+
+    public static async Task<IResponseDto<T>?> ConvertToResponseDtoAsync<T>(HttpResponseMessage httpResponse) where T : class
+    {
+        return JsonConvert.DeserializeObject<IResponseDto<T>>(await httpResponse.Content.ReadAsStringAsync());
+       
+    }
+}
---
+++ src/Cursive.Web.HttpClients/UserClient.cs
@@ -0,0 +1,46 @@
+﻿using Cursive.Communication.Dtos.Interfaces;
+using Cursive.Communication.Dtos.User.Requests;
+using Cursive.Communication.Dtos.User.Responses;
+using Cursive.Web.HttpClients.Helpers;
+using Cursive.Web.HttpClients.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Cursive.Web.HttpClients;
+
+public class UserClient : IUserClient
+{
+    private readonly IConfiguration _configuration;
+    private readonly Uri _baseUrl;
+    private readonly Uri _baseApiController;
+
+    public UserClient(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _baseUrl = new Uri(configuration["httpClient:baseUrl"] ?? throw new ArgumentNullException("The url base cannot be null."));
+        _baseApiController = new Uri(configuration["httpClient:baseApiController"] ?? throw new ArgumentNullException("The url of api cannot be null."));
+    }
+
+    public async Task<IResponseDto<UserResponse>?> CreateAsync(UserRequest userRequest)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            httpClient.BaseAddress = _baseUrl;
+
+            HttpResponseMessage httpResponse = await httpClient.PostAsync(_baseApiController, ConvertHelper.ConvertToStringContent(userRequest));
+
+            return await ConvertHelper.ConvertToResponseDtoAsync<UserResponse>(httpResponse);
+        }
+    }
+
+    public async Task<IResponseDto<TokenResponse>?> LoginAsync(LoginRequest loginRequest)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            httpClient.BaseAddress = _baseUrl;
+
+            HttpResponseMessage httpResponse = await httpClient.PostAsync($"{_baseApiController}/login", ConvertHelper.ConvertToStringContent(loginRequest));
+
+            return await ConvertHelper.ConvertToResponseDtoAsync<TokenResponse>(httpResponse);
+        }
+    }
+}
