493d5a3d7282f3593954204c313e33c7facd8818
PedroHenriqDev <devpedrohenri@gmail.com>
2025-04-21 20:08:23 -0300

    Added claim principal extension

---
+++ src/Cursive.Web.MVC/Extensions/ClaimPrincipalExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+using Cursive.Application.Services.Interfaces;
+
+namespace Cursive.Web.MVC.Extensions;
+
+public static class ClaimPrincipalExtension
+{
+    public static string GetUserId(this ClaimsPrincipal claimsPrincipal, ITokenService tokenService)
+    {
+        string token = claimsPrincipal.FindFirst("ApiToken")?.Value ?? string.Empty;
+        IList<Claim> authClaims = tokenService.GetAuthClaims(token);
+        return authClaims.FirstOrDefault(c => c.Type == "ID")?.Value ?? string.Empty;
+    }
+}
